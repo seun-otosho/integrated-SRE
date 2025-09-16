99d3c7748ad9d8ebb530b58ced91750c1df46f6e
omniotosho <toloruntotosho@gmail.com>
2025-09-16 10:41:54 +0100

    feat: Initialize Django project with Wagtail integration
    - Created base Django project structure with necessary settings files.
    - Added development and production settings for environment-specific configurations.
    - Implemented Wagtail as the CMS with initial homepage model and migrations.
    - Developed basic templates for 404 and 500 error pages, as well as a base template.
    - Set up search functionality with views and templates.
    - Added static files for CSS and JavaScript.
    - Created initial tests for homepage functionality.
    - Updated requirements.txt for Django and Wagtail dependencies.

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-)us9wc-eibrx-no^!@szokm7-19elo9z16_d8&4&r$^e*wjx$1"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
